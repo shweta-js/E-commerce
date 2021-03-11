8889914710ec73278ea127dfa50910590c57fa23
Shweta <shwetajs200@gmail.com>
2021-03-11 15:42:47 +0530

    first commit

---
+++ src/components/Cart/CartItem/styles.js
@@ -0,0 +1,19 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  media: {
+    height: 180,
+    
+  },
+  cardContent: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  cartActions: {
+    justifyContent: 'space-between',
+  },
+  buttons: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+}));
